4e20db29f04b54e5b6d759f1e2ec5fbfee401187
Vitalii Katkov <katkovvitaliy@gmail.com>
2023-12-19 22:09:19 +0200

    Removed unnecessary lazy initialization

--- src/main/java/org/thief/repository/dao/Camera.java
+++ src/main/java/org/thief/repository/dao/Camera.java
@@ -2,7 +2,6 @@ package org.thief.repository.dao;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -30,7 +29,7 @@ public class Camera {
     @Column(nullable = false, insertable = false, updatable = false)
     private Timestamp createdAt;
 
-    @OneToMany(mappedBy = "camera", fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "camera")
     @Setter(AccessLevel.NONE)
     private List<Picture> pictures;
 }
